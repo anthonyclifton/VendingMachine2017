0a3c0985ef5ce5bb495818d692e5f9ec31a07a61
Anthony Clifton <anthony@changedearth.com>
2017-04-04 23:22:35 -0500

    Cleaned up magic values and made them into constants

--- src/main/groovy/com/anthonyclifton/vendingmachine/VendingMachine.groovy
+++ src/main/groovy/com/anthonyclifton/vendingmachine/VendingMachine.groovy
@@ -3,16 +3,20 @@ package com.anthonyclifton.vendingmachine
 class VendingMachine {
     private static String INSERT_COIN = 'INSERT COIN'
 
+    private static BigDecimal DIME_VALUE = 0.10
+    private static BigDecimal QUARTER_VALUE = 0.25
+    private static BigDecimal NICKEL_VALUE = 0.05
+
     BigDecimal currentAmount = 0.0
     List<Coin> coinReturn = []
 
     void insertCoin(Coin coin) {
-        if (coin.size == 1.0 && coin.weight == 2.0) {
-            currentAmount += 0.10
-        } else if (coin.size == 3.0 && coin.weight == 4.0) {
-            currentAmount += 0.25
-        } else if (coin.size == 5.0 && coin.weight == 6.0) {
-            currentAmount += 0.05
+        if (Coin.DIME.size == coin.size && Coin.DIME.weight == coin.weight) {
+            currentAmount += DIME_VALUE
+        } else if (Coin.QUARTER.size == coin.size && Coin.QUARTER.weight == coin.weight) {
+            currentAmount += QUARTER_VALUE
+        } else if (Coin.NICKEL.size == coin.size && Coin.NICKEL.weight == coin.weight) {
+            currentAmount += NICKEL_VALUE
         } else {
             coinReturn << coin
         }
